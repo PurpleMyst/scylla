9f832c910b3a454a60a5e83fa9a59d4a7c41e8a6
Purple Myst <PurpleMyst@users.noreply.github.com>
2019-05-06 00:02:31 +0200

    feat: Add sys-ftpd module

---
+++ modules/sys_ftpd.sh
@@ -0,0 +1,20 @@
+echo_level 0 "sys-ftpd"
+
+check_devkitpro_packages switch-dev switch-mpg123
+
+echo_level 1 "Downloading"
+git clone -q https://github.com/jakibaki/sys-ftpd
+cd sys-ftpd
+
+echo_level 1 "Compiling"
+# These need to be separate because otherwise `make` considers `sys-ftpd.kip`
+# an intermediate file and delets it after it's done. Which is not what we
+# want, obviously.
+make -j$(nproc) all
+make -j$(nproc) sys-ftpd.kip
+
+echo_level 1 "Moving"
+mv sys-ftpd.kip $OUTPUT_DIR/bootloader/kip-modules/
+
+echo_level 1 "Extracting"
+cp -t $OUTPUT_DIR -r sd_card/*
