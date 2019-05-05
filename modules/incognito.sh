bf0f51a07ea0074729ad62f8ff3e2932ab579f88
Purple Myst <PurpleMyst@users.noreply.github.com>
2019-05-05 22:32:48 +0200

    feat: Add blawar/incognito module

---
+++ modules/incognito.sh
@@ -0,0 +1,8 @@
+echo_level 0 "Incognito"
+
+echo_level 1 "Downloading"
+download_latest_assets blawar incognito
+
+echo_level 1 "Moving"
+mkdir $OUTPUT_DIR/switch/incognito
+mv incognito.nro $OUTPUT_DIR/switch/incognito/
