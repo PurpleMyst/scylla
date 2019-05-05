8a4e1c8f0e5688bc25fce29dfcea167de544a440
Purple Myst <PurpleMyst@users.noreply.github.com>
2019-05-06 00:20:32 +0200

    feat: Add sys-netcheat module

---
+++ modules/sys_netcheat.sh
@@ -0,0 +1,7 @@
+echo_level 0 "sys-netcheat"
+
+echo_level 1 "Downloading"
+download_latest_assets jakibaki sys-netcheat
+
+echo_level 1 "Moving"
+mv sys-netcheat.kip $OUTPUT_DIR/bootloader/kip-modules/
