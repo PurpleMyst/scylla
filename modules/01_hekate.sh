bd527ba1144ec6933402381e685ab9094069c75c
Purple Myst <PurpleMyst@users.noreply.github.com>
2019-05-06 17:05:25 +0200

    refactor: Move hekate_ipl.ini to config/

--- modules/01_hekate.sh
+++ modules/01_hekate.sh
@@ -9,25 +9,10 @@ echo_level 1 "Downloading"
 download_latest_assets Joonie86 hekate
 
 echo_level 1 "Extracting"
-7z -y x "*hekate*.7z" -o$OUTPUT_DIR > /dev/null
+7z -y x "*hekate*.7z" -o"$OUTPUT_DIR" > /dev/null
 
 echo_level 1 "Creating KIP module dir"
-mkdir $OUTPUT_DIR/bootloader/kip-modules
+mkdir "$OUTPUT_DIR/bootloader/kip-modules"
 
 echo_level 1 "Adding config"
-cat > $OUTPUT_DIR/bootloader/hekate_ipl.ini << EOF
-[config]
-autoboot=1
-autoboot_list=0
-bootwait=3
-customlogo=1
-verification=2
-backlight=100
-autohosoff=0
-autonogc=1
-
-[Atmosphere]
-payload=bootloader/payloads/fusee-primary.bin
-kip1=bootloader/kip-modules/*
-{ }
-EOF
+cp "$CONFIG_DIR/bootloader/hekate_ipl.ini" "$OUTPUT_DIR/bootloader/"
