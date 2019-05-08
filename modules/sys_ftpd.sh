ec5786206abe324bfe671f03f4cd11843cd0514b
Purple Myst <PurpleMyst@users.noreply.github.com>
2019-05-08 23:56:46 +0200

    fix: Compile sys-ftpd to NSP. Closes #1

--- modules/sys_ftpd.sh
+++ modules/sys_ftpd.sh
@@ -1,25 +1,27 @@
 #!/usr/bin/env bash
 log-info "sys-ftpd"
 
+# Thanks to ITotalJustice for telling me the title to use for `sys-ftpd`.
+# https://github.com/PurpleMyst/scylla/issues/1#issue-441497075
+TITLE="420000000000000E"
+
 check_devkitpro_packages switch-dev switch-mpg123
 
 log-info "Cloning jakibaki/sys-ftpd"
 quiet git clone https://github.com/jakibaki/sys-ftpd || die "Could not clone jakibaki/sys-ftpd"
 cd sys-ftpd || die "sys-ftpd/ pulled out from under our feet"
 
-log-info "Compiling sys-ftpd.kip"
+log-info "Compiling sys-ftpd.nsp"
 if [ -n "$VERBOSE" ]; then
     MAKE_STDOUT="/dev/stdout"
 else
     MAKE_STDOUT="/dev/null"
 fi
-# These need to be separate because otherwise `make` considers `sys-ftpd.kip`
-# an intermediate file and delets it after it's done. Which is not what we
-# want, obviously.
-( make all && make sys-ftpd.kip ) &> "$MAKE_STDOUT" || die "Could not compile sys-ftpd.kip"
+make &> "$MAKE_STDOUT" || die "Could not compile sys-ftpd.nsp"
 
-log-info "Moving sys-ftpd.kip"
-cp sys-ftpd.kip "$OUTPUT_DIR/bootloader/kip-modules/"
+log-info "Moving sys-ftpd.nsp"
+mkdir "$OUTPUT_DIR/atmosphere/titles/$TITLE"
+cp sys-ftpd.nsp "$OUTPUT_DIR/atmosphere/titles/$TITLE/exefs.nsp" || die "Could not move sys-ftpd.nsp"
 
 log-info "Moving sd_card/*"
 cp -r sd_card/* "$OUTPUT_DIR/"
