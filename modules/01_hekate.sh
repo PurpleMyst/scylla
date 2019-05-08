fa7c71b3cbbad420ff7c67d2bfeb2e02d265176b
Purple Myst <PurpleMyst@users.noreply.github.com>
2019-05-09 00:02:32 +0200

    improvement: Put kips in /atmosphere/kips

--- modules/01_hekate.sh
+++ modules/01_hekate.sh
@@ -18,8 +18,5 @@ fi
 log-info "Extracting *hekate*.7z"
 7z -y x "*hekate*.7z" -o"$OUTPUT_DIR" > $SVNZ_STDOUT || die "Could not extract *hekate*.7z"
 
-log-info "Creating \$OUTPUT_DIR/bootloader/kip-modules"
-mkdir "$OUTPUT_DIR/bootloader/kip-modules"
-
 log-info "Adding \$OUTPUT_DIR/bootloader/hekate_ipl.ini"
 cp "$CONFIG_DIR/bootloader/hekate_ipl.ini" "$OUTPUT_DIR/bootloader/"
