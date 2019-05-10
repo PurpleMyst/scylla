f209e2b457b0153821fd62845a02a5e9759d52ac
Purple Myst <PurpleMyst@users.noreply.github.com>
2019-05-11 01:29:52 +0200

    feat: Add sys-clk module

---
+++ modules/sys_clk.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+log-info "sys-clk"
+
+log-info "Downloading assets"
+download_latest_assets retronx-team sys-clk
+
+log-info "Extracting sys-clk-*.zip"
+quiet unzip sys-clk-*.zip -d "$OUTPUT_DIR/"
+
+log-info "Removing /README.html"
+rm "$OUTPUT_DIR/README.html"
+
+log-info "Removing /config/log.flag"
+rm "$OUTPUT_DIR/config/sys-clk/log.flag"
+
+log-info "Copying \$CONFIG_DIR/config/sys-clk/"
+cp -r "$CONFIG_DIR/config/sys-clk/" "$OUTPUT_DIR/config/"
